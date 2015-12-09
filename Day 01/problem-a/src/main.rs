f903dc8e35be5cc978f1e2c1562141e7a3cfe267
ferris <jake@fusetools.com>
2015-12-09 15:32:42 -0800

    Solved first problem for Day 1

--- Day 01/problem-a/src/main.rs
+++ Day 01/problem-a/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Read;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("input.txt").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).ok();
+    let s = s;
+
+    let floor =
+        s.chars()
+        .fold(0, |acc, x| match x {
+            '(' => acc + 1,
+            ')' => acc - 1,
+            _ => unreachable!()
+        });
+
+    println!("This input will take santa to floor {}! :D", floor);
 }
