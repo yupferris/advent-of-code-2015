cb04570ca55b91130100cfdfba7115f0ecccf0a1
ferris <jake@fusetools.com>
2015-12-10 12:29:22 -0800

    Solved first problem of part 10
    A bit out of order, but it works :D

---
+++ Day 10/problem-a/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    let mut sequence = String::from("1113122113");
+
+    for _ in 0..40 {
+        sequence = look_and_say(sequence);
+    }
+
+    println!("The length is {} :D", sequence.len());
+}
+
+fn look_and_say(s: String) -> String {
+    let mut ret = String::new();
+
+    let chars = s.chars().collect::<Vec<_>>();
+    let mut index = 0;
+    let mut current_run = None;
+    while index < s.len() {
+        let next_char = chars[index];
+        current_run = match current_run {
+            Some((c, length)) => {
+                if next_char == c {
+                    index += 1;
+                    Some((next_char, length + 1))
+                } else {
+                    ret = format!("{}{}{}", ret, length, c);
+                    None
+                }
+            },
+            None => {
+                index += 1;
+                Some((next_char, 1))
+            }
+        };
+    }
+
+    if let Some((c, length)) = current_run {
+        ret = format!("{}{}{}", ret, length, c);
+    }
+
+    ret
+}
+
+#[test]
+fn a() {
+    assert_eq!(look_and_say(String::from("1")), "11");
+}
+
+#[test]
+fn b() {
+    assert_eq!(look_and_say(String::from("11")), "21");
+}
+
+#[test]
+fn c() {
+    assert_eq!(look_and_say(String::from("21")), "1211");
+}
+
+#[test]
+fn d() {
+    assert_eq!(look_and_say(String::from("1211")), "111221");
+}
+
+#[test]
+fn e() {
+    assert_eq!(look_and_say(String::from("111221")), "312211");
+}
