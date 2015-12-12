ea1ff3e166a94a6b97006c13d7246939d68b0ea2
ferris <jake@fusetools.com>
2015-12-11 21:12:26 -0800

    Solved first problem of day 12

---
+++ Day 12/problem-a/src/main.rs
@@ -0,0 +1,30 @@
+extern crate serde_json;
+
+use std::io::Read;
+use std::fs::File;
+
+use serde_json::Value;
+
+fn main() {
+    let mut f = File::open("../input.txt").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).ok();
+    let s = s;
+
+    let root: Value = serde_json::from_str(&s).unwrap();
+
+    let sum = find_sum(&root);
+
+    println!("The sum is {} :D", sum);
+}
+
+fn find_sum(value: &Value) -> i32 {
+    match value {
+        &Value::I64(x) => x as i32,
+        &Value::U64(x) => x as i32,
+        &Value::F64(x) => x as i32,
+        &Value::Array(ref values) => values.iter().fold(0, |acc, x| acc + find_sum(x)),
+        &Value::Object(ref map) => map.values().fold(0, |acc, x| acc + find_sum(x)),
+        _ => 0
+    }
+}
